9484a1106be074297032e419e8f0964e7a5a6458
Rafik <rafik.gadzhiyev.03@bk.ru>
2024-05-26 20:06:32 +0500

    feat: add new enums

--- server/helpers/enum.helper.js
+++ server/helpers/enum.helper.js
@@ -1,8 +1,22 @@
 const USER_PLANS = {
-  free: 'free',
-  premium: 'premium'
+  FREE: 'free',
+  PREMIUM: 'premium'
+}
+
+const TRANSACTION_TYPE = {
+  INCOME: 'income',
+  OUTCOME: 'outcome'
+}
+
+const TRANSACTION_STATUS = {
+  SUCCESS: 'Success',
+  FAILED: 'Failed',
+  PENDING: 'Pending'
 }
 
 module.exports = {
-  USER_PLANS
+  USER_PLANS,
+
+  TRANSACTION_TYPE,
+  TRANSACTION_STATUS
 }
